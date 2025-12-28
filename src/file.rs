91af067f00538fc2ffe42ab38f43607f7a2c622f
Kanji Tanaka <sumireminami@gmail.com>
2025-12-28 21:27:40 +0900

    Fix deserialization bug

--- src/file.rs
+++ src/file.rs
@@ -3,13 +3,11 @@ use crate::{Result, bencode::Bencode};
 use serde::Serialize;
 use std::io::Read;
 
-const HASH_SIZE: usize = 20;
-
 #[derive(Debug, Clone, Serialize)]
 pub struct Info {
     #[serde(rename = "piece length")]
     pub piece_length: u64,
-    pub pieces: Vec<Bencode>,
+    pub pieces: Bencode,
     pub name: String,
     pub length: u64,
 }
@@ -31,18 +29,13 @@ impl MetaInfo {
         let info_dict = info_bencode.as_dict()?;
 
         let piece_length = info_dict.get_int("piece length")? as u64;
-        let pieces_bytes = info_dict.get_bytes("pieces")?;
+        let pieces_bytes = info_dict.get_bytes("pieces")?.to_vec();
         let name = info_dict.get_str("name")?.to_string();
         let length = info_dict.get_int("length")? as u64;
 
-        let pieces = pieces_bytes
-            .chunks(HASH_SIZE)
-            .map(|chunk| Bencode::Str(chunk.to_vec()))
-            .collect();
-
         let info = Info {
             piece_length,
-            pieces,
+            pieces: Bencode::Str(pieces_bytes),
             name,
             length,
         };
@@ -57,14 +50,18 @@ mod tests {
     use crate::bencode::Serializer;
     use sha1::{Digest, Sha1};
 
+    const HASH_SIZE: usize = 20;
+
     #[test]
     fn test_info_serialization() {
         let info = Info {
             piece_length: 16384,
-            pieces: vec![
-                Bencode::Str(hash("hello").to_vec()),
-                Bencode::Str(hash("world").to_vec()),
-            ],
+            pieces: Bencode::Str(
+                hash("hello")
+                    .into_iter()
+                    .chain(hash("world").into_iter())
+                    .collect::<Vec<u8>>(),
+            ),
             name: "test_file.txt".to_string(),
             length: 32768,
         };
@@ -72,24 +69,23 @@ mod tests {
         let mut bytes = Vec::new();
         info.serialize(&mut Serializer::new(&mut bytes)).unwrap();
 
-        let expected = b"d6:lengthi32768e4:name13:test_file.txt12:piece lengthi16384e6:piecesl"
+        let expected = b"d6:lengthi32768e4:name13:test_file.txt12:piece lengthi16384e6:pieces"
             .iter()
-            .chain(b"20:")
+            .chain(b"40:")
             .chain(&hash("hello"))
-            .chain(b"20:")
             .chain(&hash("world"))
-            .chain(b"ee")
+            .chain(b"e")
             .cloned()
             .collect::<Vec<u8>>();
         assert_eq!(bytes, expected);
     }
 
-    fn hash(v: &str) -> [u8; HASH_SIZE] {
+    fn hash(v: &str) -> Vec<u8> {
         let mut hasher = Sha1::new();
         hasher.update(v.as_bytes());
         let result = hasher.finalize();
         let mut hash = [0u8; HASH_SIZE];
         hash.copy_from_slice(&result);
-        hash
+        hash.to_vec()
     }
 }
